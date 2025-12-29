062c17d0269e227ba13f223d69f6427ef2bd149f
Pasam Tharun <psm.thrn@gmail.com>
2025-12-29 14:46:24 +0530

    .

--- components/WelcomePage.tsx
+++ components/WelcomePage.tsx
@@ -279,7 +279,7 @@ export default function WelcomePage() {
       {/* Footer */}
       <footer className="bg-gray-900 text-gray-400 py-8">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-center">
-          <p>&copy; 2024 Calorie Tracker. Built with Next.js and Firebase.</p>
+          <p>&copy; 2025 Calorie Tracker</p>
         </div>
       </footer>
     </div>
